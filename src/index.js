45d45d348f329ea0f2cf029a77d8fc65b894875c
Alex <fastex93@gmail.com>
2015-12-07 00:54:28 +0500

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App';
+import todoApp from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(todoApp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
